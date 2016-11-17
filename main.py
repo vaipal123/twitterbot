283103ccf96abd57954c266950111f7dcf45cc8c
vaipal123 <paliwalvaibhav123@gmail.com>
2016-11-17 23:37:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import tweepy,time,sys
 
 inputFile = str(sys.argv[1])
-CONSUMER_KEY = 'vQ7AWdXJ3qjIsqLHxrXTBYbIe'
-CONSUMER_SECRET = 'MOaorEAJknz0mxZP8iDfh4QRGP8VNQUx10gVbthRfYlNS3tw0t'
-ACCESS_KEY = '120362878-K1RDF9J6Aew3Lfmbqtj25G6LwAe3y8xu2GIYsMud'
-ACCESS_SECRET = 'AZAEXkZGRq93mUAyd3XUdpRdcYRZPEZd9F47UXFWGtZuA'
+CONSUMER_KEY = 'KeyHere'
+CONSUMER_SECRET = 'SecretHere'
+ACCESS_KEY = 'AccessKeyhere'
+ACCESS_SECRET = 'accessSecretHere'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
